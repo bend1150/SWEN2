f2d4c34f1291831c625a9451e6ef1de307809235
bän <benroyong@gmail.com>
2023-05-23 22:01:21 +0200

    MapQuest
    + added EventHandler for TourList
    + added API Request
    + .png is saved in folder
    +logs

--- swen2-template/src/main/java/at/fhtw/swen2/tutorial/presentation/view/RouteListController.java
+++ swen2-template/src/main/java/at/fhtw/swen2/tutorial/presentation/view/RouteListController.java
@@ -1,5 +1,5 @@
 package at.fhtw.swen2.tutorial.presentation.view;
-
+import at.fhtw.swen2.tutorial.service.MapQuestService;
 import at.fhtw.swen2.tutorial.presentation.viewmodel.EditRouteViewModel;
 import at.fhtw.swen2.tutorial.presentation.viewmodel.RouteListViewModel;
 import at.fhtw.swen2.tutorial.service.model.Tour;
@@ -23,23 +23,40 @@ import javafx.util.Callback;
 import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import java.io.IOException;
 import javax.script.Bindings;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.MouseButton;
+import javafx.event.EventHandler;
+
+
+
 
 @Component
 public class RouteListController implements Initializable {
     @FXML
     public ListView<Tour> tourList;
+    @FXML
+    private ImageView imageView;
+
+    private static final Logger logger = LoggerFactory.getLogger(RouteListController.class);
+
 
     @Autowired
     RouteListViewModel routeListViewModel;
 
     @Autowired
     EditRouteViewModel editRouteViewModel;
+    @Autowired
+    private MapQuestService mapQuestService;
 
     public void addRoute(){
         routeListViewModel.routeCreatorPage();
@@ -98,6 +115,60 @@ public class RouteListController implements Initializable {
                 };
             }
         });
+/*
+        tourList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //check if left click
+                if(event.getButton()== MouseButton.PRIMARY && event.getClickCount()==1){
+                    String selectedTour = tourList.getSelectionModel().getSelectedItem();
+                    Tour selectedTourObject = routeListViewModel.getTours().stream().filter(t -> t.getName().equals(selectedTour)).findFirst().orElse(null);
+                    if(selectedTourObject != null){
+                        String origin = selectedTourObject.getOrigin();
+                        String destination = selectedTourObject.getDestination();
+                        System.out.println("Selected tour: " + selectedTour + ", Origin: " + origin + ", Destination: " + destination);
+                        logger.info("Getting route for origin={}, destination={}", origin, destination);
+
+
+                        try{
+                            Image tourImage = mapQuestService.getImage(origin,destination);
+                            //imageView.setImage(tourImage);
+                            imageView.setImage(tourImage);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                }
+            }
+        });*/
+
+        tourList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //check if left click
+                if(event.getButton()== MouseButton.PRIMARY && event.getClickCount()==1){
+                    Tour selectedTour = tourList.getSelectionModel().getSelectedItem();
+                    if (selectedTour != null) {
+                        String selectedTourName = selectedTour.getName();
+                        String origin = selectedTour.getOrigin();
+                        String destination = selectedTour.getDestination();
+                        System.out.println("Selected tour: " + selectedTourName + ", Origin: " + origin + ", Destination: " + destination);
+                        logger.info("Getting route for origin={}, destination={}", origin, destination);
+
+                        try{
+                            Image tourImage = mapQuestService.getImage(origin,destination);
+                            imageView.setImage(tourImage);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        });
+
+
+
     }
 
 }
